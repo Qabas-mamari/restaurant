2894d3470cc6391e28536518b4af8a4bc24d635c
Qabas-mamari <qabasalmamari@gmail.com>
2022-03-28 19:14:53 +0400

    Display Data From Database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Food; 
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        $data= food::all();  // food is the table name 
+        return view('home', compact("data"));
     }
 
     public function redirects()
     {
+        $data = food::all();
         $usertype = Auth::user()->usertype;
         if($usertype == '1'){
             return view('admin.adminhome');
         }
         else{
-            return view('home');
+            return view('home', compact("data"));
         }
     }
 }
